3f1f4e6cbe5b2e1958a090749e5d64283ee122e1
Christopher Patty <christopher.jon.patty@gmail.com>
2019-05-18 00:22:23 -0600

    Updated android theme color

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -34,13 +34,7 @@ const TemplateWrapper = ({ children }) => {
           href="/img/favicon-16x16.png"
           sizes="16x16"
         />
-
-        <link
-          rel="mask-icon"
-          href="/img/safari-pinned-tab.svg"
-          color="#f5771f"
-        />
-        <meta name="theme-color" content="#fff" />
+        <meta name="theme-color" content="#95C93D" />
 
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
