6f79549adf29eaaead37345f23e79c59ee85c57a
Chris Patty <christopher.jon.patty@gmail.com>
2019-05-21 13:19:50 -0600

    Added privacy policy to the CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,8 +3,10 @@ import styles from '../components/all.scss'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import GenericPagePreview from './preview-templates/GenericPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('privacy', GenericPagePreview)
 CMS.registerPreviewStyle(styles.toString(), { raw: true })
---
+++ src/cms/preview-templates/GenericPagePreview.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { GenericPageTemplate } from '../../templates/generic-page'
+import Environment from '../Environment'
+
+const GenericPagePreview = ({ entry, widgetFor }) => {
+  const data = entry.getIn(['data']).toJS()
+  return (
+    <Environment>
+      <GenericPageTemplate
+        content={widgetFor('body')}
+        {...data}
+      />
+    </Environment>
+  )
+}
+
+GenericPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default GenericPagePreview
