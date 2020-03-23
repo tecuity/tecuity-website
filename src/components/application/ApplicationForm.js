53f853a89d16997910181e37832bb415e7ed6422
Jaydon Carling <jaydon.carling@tecuity.com>
2020-03-23 10:57:31 -0600

    Basic drag and drop file input.

--- src/components/application/ApplicationForm.js
+++ src/components/application/ApplicationForm.js
@@ -1,5 +1,7 @@
 import React, { useState, useRef } from "react";
 import styled from "@emotion/styled";
+import DropZone from "react-dropzone";
+import dropGraphic from "../../img/file_upload.svg";
 
 export default () => {
   const [form, setForm] = useState({});
@@ -13,6 +15,19 @@ export default () => {
       ...f,
       [target.name]: { ...(f[target.name] || {}), value: target.value }
     }));
+
+  const handleFileDrop = e => {
+    e.preventDefault();
+    let files = [];
+    for (let i = 0; i < e.dataTransfer.items.length; i++) {
+      if (e.dataTransfer.items[i].kind === "file") {
+        const file = e.dataTransfer.items[i].getAsFile();
+        if (file) files.push(file);
+      }
+    }
+    if (files.length)
+      this.handleChange({ target: { files, name: this.props.fieldId } });
+  };
 
   return (
     <FormContainer>
@@ -43,6 +58,40 @@ export default () => {
           attemptedSubmit={attemptedSubmit}
           required
         />
+        {/* <DropArea htmlFor="resume-upload" onDrop={handleFileDrop}>
+          <HiddenInput type="file" id="resume-upload" />
+          <DropGraphic src={dropGraphic} />
+          <DropTextContainer>
+            <DropText>
+              Drag &amp; drop your resume here or, click to upload.
+            </DropText>
+            <DropText smaller>(.pdf or .docx)</DropText>
+          </DropTextContainer>
+        </DropArea> */}
+        <DropZone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+          {({ getRootProps, getInputProps }) => (
+            <DropArea {...getRootProps()}>
+              <input {...getInputProps()} />
+              {/* <div {...getRootProps()}>
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              </div> */}
+              <DropGraphic src={dropGraphic} />
+              <DropTextContainer>
+                <DropText>
+                  Drag &amp; drop your resume here or, click to upload.
+                </DropText>
+                <DropText smaller>(.pdf or .docx)</DropText>
+              </DropTextContainer>
+            </DropArea>
+          )}
+        </DropZone>
+        <SubmitButton
+          type="submit"
+          disabled={submitted || submitting}
+          onClick={() => setAttemptedSubmit(true)}
+        >
+          {submitting ? "Sending..." : "Submit Application"}
+        </SubmitButton>
       </Form>
     </FormContainer>
   );
@@ -54,10 +103,14 @@ const FormContainer = styled.div({
   flex: 1,
   flexDirection: "column",
   margin: "2rem 0 0 2rem",
-  padding: "1.5rem"
+  padding: "2rem"
 });
 
-const Form = styled.form();
+const Form = styled.form({
+  display: "flex",
+  flexDirection: "column",
+  height: "100%"
+});
 
 const FlexRow = styled.div({
   display: "flex",
@@ -120,7 +173,7 @@ const Field = ({
 
 const FieldWrapper = styled("div")({
   flex: 1,
-  marginBottom: 20
+  marginBottom: "1.25rem"
 });
 
 const Input = styled("input")(
@@ -155,4 +208,59 @@ const Label = styled("label")({}, ({ required }) =>
         }
       }
     : null
+);
+
+const DropArea = styled.label({
+  alignItems: "center",
+  border: "4px dashed #d0d0d0",
+  cursor: "pointer",
+  display: "flex",
+  flex: 5,
+  justifyContent: "center",
+  margin: "0.75rem 0 2rem 0",
+  minHeight: 150,
+  padding: "1rem"
+});
+
+const HiddenInput = styled.input({
+  width: 0.1,
+  height: 0.1
+});
+
+const DropTextContainer = styled.div({
+  display: "flex",
+  flex: 3,
+  flexDirection: "column"
+});
+
+const DropText = styled.span(props => ({
+  color: "#767776",
+  display: "block",
+  margin: "0.5rem 1rem",
+  fontSize: props.smaller ? "1rem" : "1.25rem",
+  fontWeight: props.smaller ? "500" : "300",
+  textAlign: "center",
+  userSelect: "none"
+}));
+
+const DropGraphic = styled.img({
+  flex: 1
+});
+
+const SubmitButton = styled("button")(
+  {
+    alignSelf: "center",
+    cursor: "pointer",
+    padding: "9px 15px 6px 15px",
+    border: "none",
+    fontSize: 20,
+    fontWeight: 500,
+    justifySelf: "flex-end",
+    maxWidth: "50%",
+    textTransform: "uppercase"
+  },
+  ({ theme }) => ({
+    background: theme.primary.color,
+    color: theme.primary.textOn
+  })
 );
